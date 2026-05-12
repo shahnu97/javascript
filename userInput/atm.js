81355be951865514c7e7f71b35aa1b98cdc14b84
Shahnawaz Khan <shahnu97@gmail.com>
2026-05-12 08:28:15 +0530

    atm and userinput

---
+++ userInput/atm.js
@@ -0,0 +1,27 @@
+//Here We are going to make ATM Machine in Javascript:
+alert('Welcome to DD International Bank PVT. LTD.');
+let sys_pin = 1234;
+let user_pin = window.prompt('Please Enter Valid PIN');
+if (sys_pin == user_pin){
+    let balance = 12000;
+    alert('Please Select the Best Option for Yourself Below Given');
+    console.log(balance);
+    let opt = window.prompt('1. Check Balance \n 2. Withdraw Amount \n 3. Deposit Money \n 4. Change Pin \n Please Enter the Required Option');
+    if(opt == 1){
+        console.log(balance);
+        alert('Your Current Balance is:' + ' ' + balance);
+    } else if (opt == 2) {
+        let amt = window.prompt('Please Enter the amount to withdraw');
+        balance = balance - amt;
+        alert('Amount Successfully Withdrawn \n Your Current Balance is' +  ' ' +  balance);
+    } else if (opt == 3) {
+        let amt = window.prompt('Please enter the amount to Deposit');
+        balance = balance + Number(amt);
+        alert('Amount Successfully Deposited \nYour Current Balance is' + ' ' + balance);
+    } else {
+        sys_pin = window.prompt('Please Enter the Updated Pin');
+        alert('Pin Changed');
+    }
+} else {
+    alert('Wrong Pin Entered');
+}
