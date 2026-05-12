2e75666be97966af573e38857e4b57b42b84d2d9
Shahnawaz Khan <shahnu97@gmail.com>
2026-05-12 08:37:06 +0530

    atm and userinput

--- userInput/atm.js
+++ userInput/atm.js
@@ -5,10 +5,8 @@ let user_pin = window.prompt('Please Enter Valid PIN');
 if (sys_pin == user_pin){
     let balance = 12000;
     alert('Please Select the Best Option for Yourself Below Given');
-    console.log(balance);
     let opt = window.prompt('1. Check Balance \n 2. Withdraw Amount \n 3. Deposit Money \n 4. Change Pin \n Please Enter the Required Option');
     if(opt == 1){
-        console.log(balance);
         alert('Your Current Balance is:' + ' ' + balance);
     } else if (opt == 2) {
         let amt = window.prompt('Please Enter the amount to withdraw');
